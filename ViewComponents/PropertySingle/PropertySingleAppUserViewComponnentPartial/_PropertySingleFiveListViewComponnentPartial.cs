bbc1598a4807bed2c326a6bf510fbc043479d533
Oğuzhan Mollamehmetoğlu <126683336+oguzhanmollamehmetoglu@users.noreply.github.com>
2024-10-15 17:26:36 +0300

    PropertySingle Emlakçıya ait diğer ilanları listelenmesine üzerinde başlandı.

---
+++ ViewComponents/PropertySingle/PropertySingleAppUserViewComponnentPartial/_PropertySingleFiveListViewComponnentPartial.cs
@@ -0,0 +1,38 @@
+﻿using DataTransferObjectLayer.Dtos.EstateAgentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Models;
+using RealEstate_Dapper_UI.Services;
+
+namespace RealEstate_Dapper_UI.ViewComponents.PropertySingle.PropertySingleAppUserViewComponnentPartial
+{
+    public class _PropertySingleFiveListViewComponnentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILoginService _loginService;
+        private readonly ApiSettings _apiSettings;
+
+        public _PropertySingleFiveListViewComponnentPartial(IHttpClientFactory httpClientFactory, ILoginService loginService, IOptions<ApiSettings> apiSettings)
+        {
+            _httpClientFactory = httpClientFactory;
+            _loginService = loginService;
+            _apiSettings = apiSettings.Value;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var id = _loginService.GetUserId;
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_apiSettings.BaseUrl);
+            var responseMessage = await client.GetAsync("EstateAgentDashboardLastProperty/GetLastFivePropertyAsync?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultDashboardLastPropertyDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
